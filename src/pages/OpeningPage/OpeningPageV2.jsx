5923f6dde1c3cbfa59edb6a7e239211c55f71b6e
KW <a0979597291@gmail.com>
2022-10-29 00:31:54 +0800

    [what] [why] [how]

--- src/pages/OpeningPage/OpeningPageV2.jsx
+++ src/pages/OpeningPage/OpeningPageV2.jsx
@@ -1,11 +1,10 @@
 import '../../css/opening_v2.scss';
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate  } from "react-router-dom";
 
 import { FiLink } from "react-icons/fi";
 import { TiUserOutline, TiKeyOutline } from "react-icons/ti";
-import { GoArrowRight, GoArrowLeft } from "react-icons/go";
 import { VscDebugDisconnect } from "react-icons/vsc";
 
 import { setUser } from "../../component/Player/userSlice";
@@ -22,10 +21,6 @@ const storage = Storage();
 
 const OpeningPageV2 = () => {
     const navigate = useNavigate();
-    const [gameMode, setGameMode] = useState("");
-    const localBtnRef = useRef(null);
-    const partyBtnRef = useRef(null);
-    const confirmBtnRef = useRef(null);
 
     let initUserName = "";
     const playerName = storage.getStorage(env.LOCAL.STORAGE.PLAYER_NAME);
@@ -37,25 +32,18 @@ const OpeningPageV2 = () => {
     const [wording, setWording] = useState("");
 
     const handleLocalBtnClick = () => {
-        localBtnRef.current.style.display = "none";
-        partyBtnRef.current.style.display = "none";
-        setGameMode("local");
+        logger.success(`Start with local mode!`);
+        dispatch(setRole("slave"));
+        let _name = formatWording("general.default.playerName", {});
+        if (userName !== "") _name = userName;
+        dispatch(setUser(_name));
+        storage.setStorage(env.LOCAL.STORAGE.PLAYER_NAME, _name);
+        storage.setStorage(env.LOCAL.STORAGE.ROOM_ID, id);
+        navigate("/local");
     };
 
     const handlePartyBtnClick = () => {
-        localBtnRef.current.style.display = "none";
-        partyBtnRef.current.style.display = "none";
-        setGameMode("party");
-    };
-
-    const handleBackBtnClick = () => {
-        localBtnRef.current.style.display = "block";
-        partyBtnRef.current.style.display = "block";
-        setGameMode("");
-    };
-
-    const handleConfirmBtnClick = () => {
-        logger.success(`Start with ${gameMode} mode!`);
+        logger.success(`Start with party mode!`);
         if (checkInputs(id)) {
             let _name = formatWording("general.default.playerName", {});
             if (userName !== "") _name = userName;
@@ -70,7 +58,7 @@ const OpeningPageV2 = () => {
             dispatch(setUser(_name));
             storage.setStorage(env.LOCAL.STORAGE.PLAYER_NAME, _name);
             storage.setStorage(env.LOCAL.STORAGE.ROOM_ID, id);
-            navigate("/"+gameMode);
+            // navigate("/party");
         } else {
             noticeWording(formatWording("error.invalid.inputRoom", {}), 1500);
         }
@@ -83,85 +71,57 @@ const OpeningPageV2 = () => {
     };
 
     const formSheet = () => {
-        if (gameMode === "local") {
-            return (
-                <>
-                    <div className="userName">
-                        <div className="userName-input-label">
-                            {formatWording("general.opening.inputName.label", {})} <TiUserOutline style = {{transform: 'translateX(2px)', fontSize: "20px"}}/>
-                        </div>
-                        <input type="text"
-                               className="userName-input"
-                               value={userName}
-                               onChange={(event) => setUserName(event.target.value)}
-                               placeholder={formatWording("general.opening.inputName.placeHolder", {})} />
+        return (
+            <>
+                <div className="userName">
+                    <div className="userName-input-label">
+                        {formatWording("general.opening.inputName.label", {})} <TiUserOutline style = {{transform: 'translateX(2px)', fontSize: "20px"}}/>
                     </div>
-                </>
-            );
-        } else if (gameMode === "party") {
-            return (
-                <>
-                    <div className="userName">
-                        <div className="userName-input-label">
-                            {formatWording("general.opening.inputName.label", {})} <TiUserOutline style = {{transform: 'translateX(2px)', fontSize: "20px"}}/>
-                        </div>
-                        <input type="text"
-                               className="userName-input"
-                               value={userName}
-                               onChange={(event) => setUserName(event.target.value)}
-                               placeholder={formatWording("general.opening.inputName.placeHolder", {})} />
+                    <input
+                        type="text"
+                        className="userName-input"
+                        value={userName}
+                        onChange={(event) => setUserName(event.target.value)}
+                        placeholder={formatWording("general.opening.inputName.placeHolder", {})}
+                    />
+                </div>
+                <div className="roomID">
+                    <div className="roomID-input-label">
+                        {formatWording("general.opening.inputRoom.label", {})} <TiKeyOutline style = {{transform: 'translateX(2px)', fontSize: "20px"}}/>
                     </div>
-                    <div className="roomID">
-                        <div className="roomID-input-label">
-                            {formatWording("general.opening.inputRoom.label", {})} <TiKeyOutline style = {{transform: 'translateX(2px)', fontSize: "20px"}}/>
-                        </div>
-                        <input type="text"
-                               className="roomID-input"
-                               value={id}
-                               onKeyDown={(event) => {
-                                   if (event.key === "Enter" && !checkInputs(id)) {
-                                       noticeWording(formatWording("error.invalid.inputRoom", {}), 1500);
-                                   }
-                               }}
-                               onChange={(event) => setId(event.target.value.slice(0, 9))}
-                               placeholder={formatWording("general.opening.inputRoom.placeHolder", {})} />
-                    </div>
-                </>
-            );
-        }
+                    <input
+                        type="text"
+                        className="roomID-input"
+                        value={id}
+                        onKeyDown={(event) => {
+                            if (event.key === "Enter" && !checkInputs(id)) {
+                                noticeWording(formatWording("error.invalid.inputRoom", {}), 1500);
+                            }
+                        }}
+                        onChange={(event) => setId(event.target.value.slice(0, 9))}
+                        placeholder={formatWording("general.opening.inputRoom.placeHolder", {})}
+                    />
+                </div>
+            </>
+        );
     };
 
 
     return (
         <div className={"container-opening"}>
             <div className={"opening-page"}>
-                {(gameMode !== "") && <div className={"goBack"} onClick={handleBackBtnClick}>
-                    <GoArrowLeft style={{transform: 'translateX(0)', fontSize: "15px", color: "gray"}}/> {formatWording("general.goBack.menu", {})}
-                </div>}
                 <div className={"page-header"}>{formatWording("general.title", {})}</div>
-                <div className={"page-content"}>
-                    <div ref={localBtnRef} className={"start-local-btn"} onClick={handleLocalBtnClick}>
-                        {formatWording("general.btn.localMode", {})} <VscDebugDisconnect style = {{transform: 'rotate(0deg) translateY(2px)', fontSize: "17px"}}/>
-                    </div>
-                    <div ref={partyBtnRef} className={"start-party-btn"} onClick={handlePartyBtnClick}>
-                        {formatWording("general.btn.partyMode", {})} <FiLink style = {{transform: 'rotate(0deg) translateY(2px)', fontSize: "14px"}}/>
-                    </div>
-                    {(gameMode !== "") && <div className={"form"}>
-                        {formSheet()}
-                        <div className={"btn-block"}>
-                            <div className={"back-btn"} onClick={handleBackBtnClick}>
-                                <div className={"temp-container"}>
-                                    <GoArrowLeft style = {{transform: 'translateX(-2px)', fontSize: "25px"}}/> {formatWording("general.btn.cancel", {})}
-                                </div>
-                            </div>
-                            <div ref={confirmBtnRef} className={"confirm-btn"} onClick={handleConfirmBtnClick}>
-                                <div className={"temp-container"}>
-                                    {formatWording("general.btn.confirm", {})} <GoArrowRight style = {{transform: 'translateX(2px)', fontSize: "25px"}}/>
-                                </div>
-                            </div>
+                <div className={"form"}>
+                    { formSheet() }
+                    <div className="wording">{wording}</div>
+                    <div className={"btn-block"}>
+                        <div className={"start-local-btn"} onClick={handleLocalBtnClick}>
+                            {formatWording("general.btn.localMode", {})} <VscDebugDisconnect style = {{transform: 'rotate(0deg) translateY(2px)', fontSize: "17px"}}/>
                         </div>
-                        <div className="wording">{wording}</div>
-                    </div>}
+                        <div className={"start-party-btn"} onClick={handlePartyBtnClick}>
+                            {formatWording("general.btn.partyMode", {})} <FiLink style = {{transform: 'rotate(0deg) translateY(2px)', fontSize: "14px"}}/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
